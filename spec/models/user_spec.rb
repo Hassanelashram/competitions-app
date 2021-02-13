18f658129b56b13125aaefa9e81ad37c3468d893
hassan el ashram <elashramhassan@gmail.com>
2021-02-13 12:43:37 +0100

    added guard

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "#full_name" do 
+  describe "#full_name" do
+    let!(:user) { create(:user, first_name: "John", last_name: "Doe") }
     it "interpolates the first name and last name" do
+      expect(user.full_name).to eq "John Doe"
     end
   end
 end
