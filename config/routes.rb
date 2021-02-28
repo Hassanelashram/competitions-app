4dc6e2521fdc563b1d552e969a44efb62b3064a8
hassan el ashram <elashramhassan@gmail.com>
2021-02-28 10:27:43 +0100

    routing to get request

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
     resources :votes, only: :create
   end
 
-  resources :votes, only: :update
+  resources :votes, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
