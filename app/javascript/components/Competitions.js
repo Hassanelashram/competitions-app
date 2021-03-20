40f855b9e2c44e9036083d70e2732286accaf4ef
hassan el ashram <elashramhassan@gmail.com>
2021-03-20 16:18:56 +0100

    added react router dom

--- app/javascript/components/Competitions.js
+++ app/javascript/components/Competitions.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Competition from "./Competition"
+
 const Competitions = ({competitions}) => {
     return(
         <div className="container">
-            {console.log("1", competitions)}
             <div className="row">
                 { competitions.map(competition => <Competition key={competition.id} competition={competition}/>)}
              </div>
