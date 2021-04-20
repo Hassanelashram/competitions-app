cec6a60e77700965526df27972caabd97ed98360
hassan el ashram <elashramhassan@gmail.com>
2021-04-20 15:10:59 +0200

    copy clipboard through tubrolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,16 +31,17 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  const url = "https://stryfe-app.herokuapp.com/users/sign_up?referrer=";
-    const copyLinkToClipboard = (user_id) => {
-      navigator.clipboard.writeText(url + user_id).then(function() {
-        const linkBtn = document.getElementById("link-btn");
+    const linkBtn = document.getElementById("link-btn");
+    linkBtn.addEventListener('click', () => {
+      const url = "https://stryfe-app.herokuapp.com/users/sign_up?referrer=";
+      navigator.clipboard.writeText(url + linkBtn.dataset.userId).then(function() {
         linkBtn.textContent = "Link Copied"
         linkBtn.classList.add("disabled")
       }, function(err) {
         console.error('Async: Could not copy text: ', err);
       });
-    }
+    })
+      
     
   let countryBtn = document.getElementById('country-btn');
   const chart = document.getElementById('chart-views');
